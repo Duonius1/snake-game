a71849ae1e3f28ee64e172d3f33dc5cb467e8bf3
Duonius1 <mindaugas.ramonas9@gmail.com>
2025-02-06 20:56:43 +0200

    Deletion

---
+++ Snake-game.py
@@ -0,0 +1,30 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    # Move the last segment to where the second to last segment is located until the first segment
+    snake.move()
+
+
+
+screen.exitonclick()
